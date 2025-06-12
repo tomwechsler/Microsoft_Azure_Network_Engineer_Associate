4b209636f9c813b6c58b34e1357dc8d978393f98
Tom Wechsler <tom.wechsler@outlook.com>
2025-06-12 07:09:51 +0200

    New scripts

---
+++ 03_Design_implement_name_resolution/03_01_vnet_buildout.ps1
@@ -0,0 +1,44 @@
+###############################
+####### SCRIPT DETAILS ########
+# Intended Purpose: Setup Azure environment
+# Disclaimer: This script is intended to be used only for learning purposes.
+# Message: To use this script for your own Azure environments
+#       1.) Create your own resource group variable.
+#       2.) Comment out variable in variables section.
+#       3.) Uncomment below commands and assign your own resource group and location.
+# rg=your-resource-group-here
+# location=your-location-here
+###############################
+
+##############################
+##### START - VARIABLES ######
+##############################
+
+# Get resource group and set to variable $rg
+$rg = az group list --query '[].name' -o tsv
+
+# Assign location variable to playground resource group location
+$location = az group list --query '[].location' -o tsv
+
+##############################
+##### END - VARIABLES ######
+##############################
+
+
+##############################
+####### START - SCRIPT #######
+##############################
+
+# Create Virtual Network and subnet
+
+az network vnet create --name cake-hub-vnet --resource-group $rg --location $location --address-prefixes 10.0.0.0/16 --subnet-name hub-subnet-01 --subnet-prefix 10.0.1.0/24
+
+# Create two Linux machines. We will not need to interact with the machines, just generate IP/DNS info
+
+az vm create --resource-group $rg --name vm-1 --image Ubuntu2204 --generate-ssh-keys --public-ip-address myPublicIP-vm1 --public-ip-sku Standard --vnet-name cake-hub-vnet --subnet hub-subnet-01 --size Standard_B1s --no-wait
+
+az vm create --resource-group $rg --name vm-2 --image Ubuntu2204 --generate-ssh-keys --public-ip-address myPublicIP-vm2 --public-ip-sku Standard --vnet-name cake-hub-vnet --subnet hub-subnet-01 --size Standard_B1s --no-wait
+
+##############################
+######## END - SCRIPT ########
+##############################
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Azure Network Engineer Associate (AZ-700)
-Everything about Microsoft Azure Networking!
+
+## Description
+This repository contains resources, notes, and practice materials for the Microsoft Azure Network Engineer Associate certification (AZ-700). It is designed to help candidates prepare for the exam by providing a comprehensive overview of Azure networking concepts, services, and best practices.
+
+### Disclaimer
+**The scripts in this repository are for educational purposes only. The scripts in this repository serve as a starting point, the final configuration must still be made (in the portal or further with scripts).**   
+
+## Pre-requisites
+- Basic understanding of networking concepts
+- Familiarity with Microsoft Azure services
+- Experience with Azure portal and CLI
