654ac08ec8050e74a8ff4fbe6a62703949698140
Tom Wechsler <tom.wechsler@outlook.com>
2025-06-24 06:58:29 +0200

    New exercise

---
+++ 06_Implement_and_design_network_security_groups/06_02_asg_buildout.ps1
@@ -0,0 +1,101 @@
+###############################
+####### SCRIPT DETAILS ########
+# Intended Purpose: Setup Azure environment
+# Disclaimer: This script is intended to be used only for learning purposes.
+# Message: To use this script for your own Azure environments
+#       1.) Create your own resource group variable.
+#       2.) Comment out variable in variables section.
+#       3.) Uncomment below commands and assign your own resource group and location.
+# rg=your-resource-group-here
+# location=your-location-here
+###############################
+
+##############################
+##### START - VARIABLES ######
+##############################
+
+# Get resource group and set to variable $rg
+$rg = az group list --query '[].name' -o tsv
+
+# Assign location variable to playground resource group location
+$location = az group list --query '[].location' -o tsv
+
+##############################
+##### END - VARIABLES ######
+##############################
+
+
+##############################
+####### START - SCRIPT #######
+##############################
+
+
+## CLOUD INIT FOR USERDATA SETUP OF VM
+curl https://raw.githubusercontent.com/tomwechsler/Microsoft_Azure_Network_Engineer_Associate/refs/heads/main/06_Implement_and_design_network_security_groups/cloud-init.txt -o cloud-init.txt
+
+## SETUP MAIN HUB VNET
+# Create main hub vnet
+az network vnet create --name cake-hub-vnet --resource-group $rg --location $location --address-prefixes 10.60.0.0/16 --subnet-name hub-subnet-01 --subnet-prefix 10.60.0.0/24
+
+# Create nsg-01
+az network nsg create -g $rg -n hub-nsg-01
+
+# Associate nsg-01 with subnet-01 in main hub vnet
+az network vnet subnet update --resource-group $rg --vnet-name cake-hub-vnet --name hub-subnet-01 --network-security-group hub-nsg-01
+
+# Create nsg-01 rules allow SSH|HTTP from Anywhere
+az network nsg rule create --resource-group $rg --nsg-name hub-nsg-01 --name allowHttp --priority 110 --destination-port-ranges 80 --source-address-prefixes '*' --access Allow --protocol Tcp
+az network nsg rule create --resource-group $rg --nsg-name hub-nsg-01 --name denySsh --priority 120 --destination-port-ranges 22 --source-address-prefixes '*' --access Deny --protocol Tcp
+
+# Create vm-1 in main hub vnet subnet-01
+az vm create --resource-group $rg --location $location --name cake-hub-vm-01 --image Ubuntu2204 --admin-username azureuser --generate-ssh-keys --user-data ./cloud-init.txt --public-ip-address cake-hub-pip-01 --public-ip-sku Standard --vnet-name cake-hub-vnet --subnet hub-subnet-01 --nsg hub-nsg-01 --size Standard_B1s --no-wait
+
+# Create vm-2 in main hub vnet subnet-01
+az vm create --resource-group $rg --location $location --name cake-hub-vm-02 --image Ubuntu2204 --admin-username azureuser --generate-ssh-keys --user-data ./cloud-init.txt --public-ip-address cake-hub-pip-02 --public-ip-sku Standard --vnet-name cake-hub-vnet --subnet hub-subnet-01 --nsg hub-nsg-01 --size Standard_B1s --no-wait
+
+
+## SETUP SPOKE 1 VNET
+# Create spoke 1 vnet
+az network vnet create --name cake-spoke1-vnet --resource-group $rg --location $location  --address-prefixes 10.120.0.0/16 --subnet-name spoke1-subnet-01 --subnet-prefix 10.120.0.0/24
+
+# Create nsg-01
+az network nsg create -g $rg -n spoke1-nsg-01
+
+# Associate nsg-01 with subnet-01 in spoke 1 hub vnet
+az network vnet subnet update --resource-group $rg --vnet-name cake-spoke1-vnet --name spoke1-subnet-01 --network-security-group spoke1-nsg-01
+
+# Create nsg-01 rules allow SSH|HTTP from Anywhere
+az network nsg rule create --resource-group $rg --nsg-name spoke1-nsg-01 --name allowHttp --priority 110 --destination-port-ranges 80 --source-address-prefixes '*' --access Allow --protocol Tcp
+az network nsg rule create --resource-group $rg --nsg-name spoke1-nsg-01 --name denySsh --priority 120 --destination-port-ranges 22 --source-address-prefixes '*' --access Deny --protocol Tcp
+
+# Create vm-1 in spoke 1 vnet subnet-01
+az vm create --resource-group $rg --location $location --name cake-spoke1-vm-01 --image Ubuntu2204 --admin-username azureuser --generate-ssh-keys --user-data ./cloud-init.txt --public-ip-address cake-spoke1-pip-01 --public-ip-sku Standard --vnet-name cake-spoke1-vnet --subnet spoke1-subnet-01 --nsg spoke1-nsg-01 --size Standard_B1s --no-wait
+
+# Create vm-2 in spoke 1 vnet subnet-01
+az vm create --resource-group $rg --location $location --name cake-spoke1-vm-02 --image Ubuntu2204 --admin-username azureuser --generate-ssh-keys --user-data ./cloud-init.txt --public-ip-address cake-spoke1-pip-02 --public-ip-sku Standard --vnet-name cake-spoke1-vnet --subnet spoke1-subnet-01 --nsg spoke1-nsg-01 --size Standard_B1s --no-wait
+
+
+## SETUP SPOKE 2 VNET
+# Create spoke 2 vnet
+az network vnet create --name cake-spoke2-vnet --resource-group $rg --location $location  --address-prefixes 172.32.0.0/16 --subnet-name spoke2-subnet-01 --subnet-prefix 172.32.0.0/24
+
+# Create nsg-01
+az network nsg create -g $rg -n spoke2-nsg-01
+
+# Associate nsg-01 with subnet-01 in spoke 2 hub vnet
+az network vnet subnet update --resource-group $rg --vnet-name cake-spoke2-vnet --name spoke2-subnet-01 --network-security-group spoke2-nsg-01
+
+# Create nsg-01 rules allow SSH|HTTP from Anywhere
+az network nsg rule create --resource-group $rg --nsg-name spoke2-nsg-01 --name allowHttp --priority 110 --destination-port-ranges 80 --source-address-prefixes '*' --access Allow --protocol Tcp
+az network nsg rule create --resource-group $rg --nsg-name spoke2-nsg-01 --name denySsh --priority 120 --destination-port-ranges 22 --source-address-prefixes '*' --access Deny --protocol Tcp
+
+# Create vm-1 in spoke 2 vnet subnet-01
+az vm create --resource-group $rg --location $location --name cake-spoke2-vm-01 --image Ubuntu2204 --admin-username azureuser --generate-ssh-keys --user-data ./cloud-init.txt --public-ip-address cake-spoke2-pip-01 --public-ip-sku Standard --vnet-name cake-spoke2-vnet --subnet spoke2-subnet-01 --nsg spoke2-nsg-01 --size Standard_B1s --no-wait
+
+# Create vm-2 in spoke 2 vnet subnet-01
+az vm create --resource-group $rg --location $location --name cake-spoke2-vm-02 --image Ubuntu2204 --admin-username azureuser --generate-ssh-keys --user-data ./cloud-init.txt --public-ip-address cake-spoke2-pip-02 --public-ip-sku Standard --vnet-name cake-spoke2-vnet --subnet spoke2-subnet-01 --nsg spoke2-nsg-01 --size Standard_B1s --no-wait
+
+
+##############################
+######## END - SCRIPT ########
+##############################
